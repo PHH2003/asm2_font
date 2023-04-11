92804482ff74f49a30b98cc5dd9093fa0b93b3ed
hiep <phanhuyhiep2k3@gmail.com>
2023-04-11 22:41:00 +0700

    update login, delete

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,5 +1,7 @@
+import { json } from "react-router-dom";
 import Iproduct from "../interface/product";
 import instance from "./instance";
+const {accessToken}=JSON.parse(localStorage.getItem("user")!)
 export const getAll=()=>{
     return instance.get("/products")
 }
@@ -10,7 +12,11 @@ export const addproduct=(data:Iproduct)=>{
     return instance.post("/products",data)
 }
 export const deleteproduct=(id:string)=>{
-    return instance.delete("/products/"+id)
+    return instance.delete("/products/"+id,{
+        headers:{
+            Authorization:`Bearer ${accessToken}`
+        }
+    })
 }
 export const updateproduct=(data:Iproduct)=>{
     return instance.put("/products/"+data._id,data)
--- src/pages/admin/ListProduct.tsx
+++ src/pages/admin/ListProduct.tsx
@@ -1,5 +1,5 @@
 import React ,{ useEffect, useState }from 'react'
-import { Table } from 'antd';
+import { Table ,Button} from 'antd';
 import type { ColumnGroupType,ColumnType } from 'antd/es/table';
 import type { TableRowSelection } from 'antd/es/table/interface';
 import Iproduct from '../../interface/product';
@@ -12,31 +12,44 @@ interface DataType {
 }
 interface Iprops{
     product:Iproduct[]
+    onRemove:Function
 }
-const columns: ColumnGroupType<Iproduct> | ColumnType<Iproduct>[] = [
-  {
-    title: 'name',
-    dataIndex: 'name',
-  },
-  {
-    title: 'price',
-    dataIndex: 'price'
-  }
-];
+
 
 
 const ListProduct = (props:Iprops) => {
   
     const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [data,setData]=useState<Iproduct[]>([])
+  console.log(data)
   useEffect(()=>{
     setData(props.product)
-  },[])
+  },[props])
+
+   const handelRemove=(id:string)=>{
+    console.log(id)
+    props.onRemove(id)
+   }
   const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
     console.log('selectedRowKeys changed: ', newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
   };
-
+  const columns: ColumnGroupType<Iproduct> | ColumnType<Iproduct>[] = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'price',
+      dataIndex: 'price'
+    },
+    {
+      title:"Action",
+      render:(_,record)=>(
+        <Button type="primary" onClick={()=>handelRemove(record._id)}>Delete</Button>
+      )
+    }
+  ];
   const rowSelection: TableRowSelection<Iproduct> = {
     selectedRowKeys,
     onChange: onSelectChange,
